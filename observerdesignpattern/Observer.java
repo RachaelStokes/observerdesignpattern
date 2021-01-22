e7c79fed089abda0f01828d71fb955e8406011fb
Rachael Stokes <rachaelstokes@Rachaels-MacBook-Pro-2.local>
2021-01-22 13:27:34 -0500

    javadoc for Observer interface

--- observerdesignpattern/Observer.java
+++ observerdesignpattern/Observer.java
@@ -5,5 +5,11 @@ package observerdesignpattern;
  * @author Rachael
  */
 public interface Observer {
+
+    /**
+     * method that updates the current value of strokes and par for the golfer
+     * @param strokes the int that keeps track of the strokes for a hole
+     * @param par the int that keeps track of the par for a hole
+     */
     public void update(int strokes, int par);
 }
