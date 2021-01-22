3cb45ae358559789fa83f0b2449895cc1b1ffb37
Rachael Stokes <rachaelstokes@Rachaels-MacBook-Pro-2.local>
2021-01-22 13:45:25 -0500

    javadoc to round score display

--- observerdesignpattern/RoundScoreDisplay.java
+++ observerdesignpattern/RoundScoreDisplay.java
@@ -1,24 +1,37 @@
 package observerdesignpattern;
 /**
- * 
+ * class to display the round hole stats and implements the Observer interface
  * @author Rachael
  */
 public class RoundScoreDisplay implements Observer{
-    private Subject golfer;
-    private int strokesTotal;
-    private int parTotal;
+    //instance variables
+    private Subject golfer; //new subject golfer
+    private int strokesTotal; //int to keep track of total strokes
+    private int parTotal; //int to keep track of total par
 
+    /**
+     * sets the golfer and calls register observer to add the golfer to the list of observers
+     * @param golfer the subject that is being registered as an observer
+     */
     public RoundScoreDisplay(Subject golfer) {
         this.golfer = golfer;
         golfer.registerObserver(this);
     }
 
+    /**
+     * adds the strokes and the par to the totals and calls the displayRoundScore method
+     * @param strokes int used to add to the total strokes
+     * @param par int used to add to the total par
+     */
     public void update(int strokes, int par) {
         this.strokesTotal += strokes;
         this.parTotal += par;
         displayRoundScore();
     }
 
+    /**
+     * method prints out the round stats and how many above or below par the golf is or if they made par
+     */
     private void displayRoundScore() {
         int score;
         System.out.println(" ");
